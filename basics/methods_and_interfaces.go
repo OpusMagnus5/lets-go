85c7e327b0549bc649938e0dbeb5040d48e4c193
Damian Bodzioch <damianbodzioch95@gmail.com>
2025-08-21 15:03:28 +0200

    interfaces

--- basics/methods_and_interfaces.go
+++ basics/methods_and_interfaces.go
@@ -8,6 +8,7 @@ import (
 func TestMethodsAndInterfaces() {
 	fmt.Println("--methods and interfaces----------------------------------------------------------------------------")
 	methods()
+	interfaces()
 }
 
 type Coordinates struct {
@@ -64,5 +65,57 @@ func methods() {
 	v2 := Coordinates{3, 4}
 	v2.scale(10)
 	fmt.Println(v2.abs())
+}
+
+/*
+Typ klasy interfejs jest definiowany przez zbiór sygnatur jego metody.
+Typ implementuje interfejs implementując jego metody. Nie ma tutaj jawnej deklaracji tworzącej dany typ klasy interfejs, takiej jak na przykład „implements”.
+Domyślne tworzenie interfejsu rozdziela definicje interfejsu od jego implementacji, które mogą się potem pojawić w dowolnym pakiecie
+*/
+type I interface {
+	m()
+}
+
+type I2 interface {
+	m2()
+}
+
+type T struct {
+	s string
+}
+
+func (t T) m() {
+	fmt.Println(t.s)
+}
+
+/*
+Jeśli konkretna wartość w interfejsie to nil, metoda zostanie wywołana z odbiorcą nil.
+*/
+func (t *T) m2() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(t.s)
+}
+
+/*
+Wartość nil interfejsu nie przetrzymuje ani konkretnej wartości ani żadnego konkretnego typu.
+Wezwanie metody na wartości interfejsu będącą nil powoduje błąd w czasie wykonania programu (ang. run-time error),
+*/
+
+/*
+„Pusty interfejs” to typ interfejs który nie określa żadnej metody: interface{}
+Pusty interfejs może przetrzymywać wartości każdego typu. (Każdy typ implementuje co najmniej zero metod.)
+Puste interfejsy używane są przez kod który obsługuje wartości nieznanych typów. Na przykład, fmt.Print przyjmuje jakakolwiek liczbę argumentów typu interface{}.
+*/
+
+func interfaces() {
+	var i I = T{"witaj"}
+	i.m()
+
+	var t2 *T
+	var i2 I2 = t2
+	i2.m2()
 }
 
